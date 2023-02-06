cddc0a3f51bf958859ceea637626526fd186714a
PriyanshuChourasia <105114609+PriyanshuChourasia@users.noreply.github.com>
2023-02-06 13:42:21 +0530

    m

--- database/migrations/2022_11_08_234612_create_collection_centres_table.php
+++ database/migrations/2022_11_08_234612_create_collection_centres_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->string('email')->unique();
+            $table->string('code')->unique()->nullable();
             $table->string('password')->default(bcrypt('password'));
             $table->string('address')->nullable();
             $table->string('contact_no')->nullable();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call(AdminSeeder::class);
+        // $this->call(AdminSeeder::class);
+        $this->call(EmployeeSeeder::class);
     }
 }
